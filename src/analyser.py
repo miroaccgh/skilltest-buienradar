09a49b8ea6e0e95757d0eab3652da3fd9e6c898d
miroaccgh <wabysaby@proton.me>
2025-03-28 13:14:01 +0100

    setup outline for api

--- src/analyser.py
+++ src/analyser.py
@@ -1,6 +1,8 @@
 import database
+import api
 
 class Analyser:
     def __init__(self) -> None:
         self._db_client = database.Client()
         self._db_client.query_all_instances()
+        self._api_client  = api.Client()
